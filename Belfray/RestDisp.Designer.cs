700af3115a28490acd27b2ea943e054a460d649e
SK <kel17108239@B505-003>
2020-03-11 09:36:41 +0000

    Updated Main

---
+++ Belfray/RestDisp.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Belfray
+{
+    partial class RestDisp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvRestStock = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRestStock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvRestStock
+            // 
+            this.dgvRestStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRestStock.Location = new System.Drawing.Point(-206, 54);
+            this.dgvRestStock.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvRestStock.Name = "dgvRestStock";
+            this.dgvRestStock.RowTemplate.Height = 28;
+            this.dgvRestStock.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvRestStock.Size = new System.Drawing.Size(1213, 343);
+            this.dgvRestStock.TabIndex = 1;
+            this.dgvRestStock.Visible = false;
+            // 
+            // RestDisp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvRestStock);
+            this.Name = "RestDisp";
+            this.Text = "RestDisp";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRestStock)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvRestStock;
+    }
+}
