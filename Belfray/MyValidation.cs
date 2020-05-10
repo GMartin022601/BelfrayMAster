1a087d8ce852c35e2df02e48b3d45f9d7bf18d46
Martin, Gary <Gary.Martin@fujitsu.com>
2020-05-10 21:52:05 +0100

    gm

--- Belfray/MyValidation.cs
+++ Belfray/MyValidation.cs
@@ -96,7 +96,7 @@ namespace Belfray
             return ok;
         }
         //Valid Forename
-        public static bool validForename(string txt) //allows aphatbetic, dash and whitespace
+        public static bool validForename(string txt) //allows aphatbetic and dash
         {
             bool ok = true;
 
@@ -108,7 +108,7 @@ namespace Belfray
             {
                 for (int x = 0; x < txt.Length; x++)
                 {
-                    if (!(char.IsLetter(txt[x])) && !(char.IsWhiteSpace(txt[x])) && !(txt[x].Equals('-')))
+                    if (!(char.IsLetter(txt[x])) && !(txt[x].Equals('-')))
                         ok = false;
                 }
             }
@@ -127,7 +127,7 @@ namespace Belfray
             {
                 for (int x = 0; x < txt.Length; x++)
                 {
-                    if (!(char.IsLetter(txt[x])) && !(char.IsWhiteSpace(txt[x])) && !(txt[x].Equals('-')))
+                    if (!(char.IsLetter(txt[x])) && !(txt[x].Equals('-')))
                     {
                         ok = false;
                     }
